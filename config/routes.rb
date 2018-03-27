0c117a015247088d2baeba4ede0fe5bb69c0172a
Elliot Seror <elliot.seror@edhec.com>
2018-03-27 22:43:30 +0100

    let's put this on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'investor', to: 'pages#investor', as: :investor
+  get 'manager', to: 'pages#manager', as: :manager
+    get 'principles', to: 'pages#principles', as: :principles
+
 end
